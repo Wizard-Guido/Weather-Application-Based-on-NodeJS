965df428d4e6bc61e6f658dc9b1d26d7a5c0b793
WilliamKoo <william.w.gu@outlook.com>
2022-04-02 18:10:20 +0800

    test some issue

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,18 @@ weatherForm.addEventListener('submit', (e) => {
     if (!location.length) return message1.textContent = 'Please provide the location!';
     message1.textContent = 'Loading...';
     message2.textContent = '';
-    fetch(`/weather?search=${location}`).then((response) => {
+    // fetch(`/weather?search=${location}`).then((response) => {
+    //     response.json().then((data) => {
+    //         if (data.error) return message1.textContent = 'Please provide the valid location!';
+    //         message1.textContent = `city: ${data.city}, country: ${data.country}`;
+    //         message2.textContent = `Tips: ${data.tips}`;
+    //     })
+    // });
+    fetch(`http://api.weatherstack.com/current?access_key=23cf270ceec711762d56f9d1ac5b5e2f&query=${location}`, {headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'}}).then((response) => {
         response.json().then((data) => {
             if (data.error) return message1.textContent = 'Please provide the valid location!';
-            message1.textContent = `city: ${data.city}, country: ${data.country}`;
-            message2.textContent = `Tips: ${data.tips}`;
+            message1.textContent = `city: ${data.location.city}, country: ${data.location.country}`;
+            message2.textContent = `Temperature: ${data.current.temperature}, Wind Speed: ${data.current.wind_speed}`;
         })
     });
 })
